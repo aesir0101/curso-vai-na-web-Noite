ed22a76c3db626d99ab6a87b823291e343226e9b
aesir <84733192+aesir0101@users.noreply.github.com>
2025-10-02 13:57:56 -0300

    Update main.js

--- catalogo flm/main.js
+++ catalogo flm/main.js
@@ -1,8 +1,23 @@
-// MENU MOBILE
-const hamburger = document.getElementById('hamburger');
-const mobileMenu = document.getElementById('mobileMenu');
+    $(document).ready(function () {
+      const toggleButton = $('#hamburger');
+      const mobileMenu = $('#mobileMenu');
+      const body = $('body');
 
-hamburger.addEventListener('click', () => {
-  mobileMenu.classList.toggle('active');
-  document.body.classList.toggle('menu-open');
-});
+      toggleButton.click(function () {
+        mobileMenu.toggleClass('active');
+        body.toggleClass('menu-open');
+      });
+
+      $('#mobileMenu a').click(function () {
+        mobileMenu.removeClass('active');
+        body.removeClass('menu-open');
+      });
+
+      $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 50) {
+          $('.navbar').css('background', 'rgba(44, 44, 44, 0.6)');
+        } else {
+          $('.navbar').css('background', '#2c2c2c');
+        }
+      });
+    });
